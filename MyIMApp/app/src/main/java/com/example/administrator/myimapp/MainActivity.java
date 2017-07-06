ae211bf98cf53254d6d265647af924928db950c4
Your Name <you@example.com>
2017-07-06 18:13:46 +0800

    提交
    提交

--- MyIMApp/app/src/main/java/com/example/administrator/myimapp/MainActivity.java
+++ MyIMApp/app/src/main/java/com/example/administrator/myimapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.myimapp;
 
+import android.media.audiofx.LoudnessEnhancer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
