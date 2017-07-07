53f8030e0e65d7e4fc71cb9d0f08fd76073a3e30
Your Name <you@example.com>
2017-07-07 18:58:30 +0800

    提交

--- IMAppServer/src/main/java/net/qiujuer/web/italker/push/Application.java
+++ IMAppServer/src/main/java/net/qiujuer/web/italker/push/Application.java
@@ -1,7 +1,25 @@
 package net.qiujuer.web.italker.push;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.qiujuer.web.italker.push.service.AccountService;
+import org.glassfish.jersey.jaxb.internal.XmlJaxbElementProvider;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
 /**
  * Created by Administrator on 2017/7/7 0007.
+ * @author hfrx
  */
-public class Application {
+public class Application extends ResourceConfig {
+    public Application(){
+        //注册逻辑处理的包名
+        packages(AccountService.class.getPackage().getName());
+
+        //注册Json解析器
+        register(JacksonJsonProvider.class);
+
+        //注册日志打印输出
+        register(Logger.class);
+    }
 }
---
+++ IMAppServer/src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
@@ -0,0 +1,13 @@
+package net.qiujuer.web.italker.push.service;
+
+import javax.ws.rs.Path;
+
+/**
+ * @author HFRX
+ */
+//127.0.0.1/api/account
+@Path("/account")
+public class AccountService {
+
+
+}
