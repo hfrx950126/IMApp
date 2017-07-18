6a2057f73706f51ceb8066c96748d603dbbaa1fa
Your Name <you@example.com>
2017-07-18 18:45:15 +0800

    提交

--- ITalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
+++ ITalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
@@ -1,13 +1,77 @@
 package net.qiujuer.italker.push;
 
+
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
+import net.qiujuer.genius.ui.widget.FloatActionButton;
+
+import net.qiujuer.italker.common.app.Activity;
+import net.qiujuer.italker.common.widget.PortraitView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity {
+
+    @BindView(R.id.appbar)
+    View mLayAppbar;
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
+
+    @BindView(R.id.btn_action)
+    FloatActionButton mAction;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+        Glide.with(this).load(R.drawable.bg_src_morning).centerCrop().into(new ViewTarget<View,GlideDrawable>(mLayAppbar) {
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                this.view.setBackground(resource.getCurrent());
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick(){
+
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick() {
+
+    }
+
+
 }
---
+++ ITalker/common/src/main/java/net/qiujuer/italker/common/widget/PortraitView.java
@@ -0,0 +1,24 @@
+package net.qiujuer.italker.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Administrator on 2017/7/18 0018.
+ */
+
+public class PortraitView extends CircleImageView {
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
